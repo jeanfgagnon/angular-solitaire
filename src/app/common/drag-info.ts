a5df9450df3d084dbfff7960d8c8b9f8ca887621
JFG <lapuff@yahoo.com>
2020-04-25 16:53:50 -0400

    Drag info extracted into dto

---
+++ src/app/common/drag-info.ts
@@ -0,0 +1,6 @@
+export default class DragInfo {
+  public draggedFrom: 'deck' | 'col';
+  public draggedFromIndex: number;
+  public draggedTo: 'sky' | 'col';
+  public draggedToIndex: number;
+}
--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CardDeckService } from 'src/app/services/card-deck.service';
 import { CardModel } from 'src/app/common/card-model';
 import { CardFaces } from 'src/app/common/card-faces.enum';
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
+import DragInfo from 'src/app/common/drag-info';
 
 @Component({
   selector: 'app-board',
@@ -35,11 +36,55 @@ export class BoardComponent implements OnInit {
 
   // event handlers
 
-  drop(event: CdkDragDrop<string[]>) {
-    console.dir(event);
+  public drop(event: CdkDragDrop<string[]>) {
+    // first we can't drag from sky
+    const dragInfo: DragInfo = this.getDraggedInfo(event);
+
+    console.log('from %s', JSON.stringify(dragInfo));
   }
-  
+
   // private code
+
+  private getDraggedInfo(e: CdkDragDrop<string[], string[]>): DragInfo {
+    const rv = new DragInfo();
+
+    // from
+    if (e.previousContainer.id.startsWith('col')) {
+      rv.draggedFrom = 'col';
+    }
+    else if (e.previousContainer.id.startsWith('deck')) {
+      rv.draggedFrom = 'deck';
+    }
+    rv.draggedFromIndex = this.getIndexFromId(e.previousContainer.id);
+
+    // to
+    if (e.container.id.startsWith('col')) {
+      rv.draggedTo = 'col';
+    }
+    else if (e.container.id.startsWith('sky')) {
+      rv.draggedTo = 'sky';
+    }
+    rv.draggedToIndex = this.getIndexFromId(e.container.id);
+
+    return rv;
+  }
+
+  getIndexFromId(id: string): number {
+    let rv = -1;
+    try {
+      if (id.startsWith('col') || id.startsWith('sky')) {
+        rv = parseInt(id.substr(3));
+      }
+      else {
+        rv = parseInt(id.substr(4));
+      }
+    }
+    catch {
+      console.log('ID of unknown type (%s)', id);
+    }
+
+    return rv;
+  }
 
   private prepPiles(): void {
     this.prepColPiles();
@@ -66,7 +111,7 @@ export class BoardComponent implements OnInit {
           // turn top card
           this.deck[deckIndex].Open = true;
         }
-        
+
         this.deck[deckIndex].Coords.xPos = 0;
 
         if (cardIndex > 0) {
